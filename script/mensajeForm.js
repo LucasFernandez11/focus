88cc2693cfa62c3a208a9c2c1f2720a3a042a786
lucas fernandez <lucas.fernamdez.lmf11@gmail.com>
2023-09-03 00:02:00 -0300

    cambio en la logica del mensaje

--- script/mensajeForm.js
+++ script/mensajeForm.js
@@ -1,43 +1,40 @@
- // Función para mostrar el mensaje emergente
- function mostrarMensaje(mensaje, esExitoso) {
-    var mensajeTexto = document.getElementById("mensajeTexto");
-    var mensajePopup = document.getElementById("mensajePopup");
 
-    mensajeTexto.innerText = mensaje;
+    // Función para mostrar el mensaje emergente
+    function mostrarMensaje(mensaje, esExitoso) {
+        var mensajeTexto = document.getElementById("mensajeTexto");
+        var mensajePopup = document.getElementById("mensajePopup");
 
-    if (esExitoso) {
-        mensajePopup.style.backgroundColor = "green";
-    } else {
-        mensajePopup.style.backgroundColor = "red";
+        mensajeTexto.innerText = mensaje;
+
+        if (esExitoso) {
+            mensajePopup.style.backgroundColor = "green";
+        } else {
+            mensajePopup.style.backgroundColor = "red";
+        }
+
+        mensajePopup.style.display = "block";
+
+        // Ocultar el mensaje después de 5 segundos
+        setTimeout(function () {
+            mensajePopup.style.display = "none";
+        }, 5000); // 5000 milisegundos = 5 segundos
     }
 
-    mensajePopup.style.display = "block";
-}
+    // Manejar la respuesta del formulario
+    var form = document.querySelector(".form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-// Función para cerrar el mensaje emergente
-function cerrarMensaje() {
-    var mensajePopup = document.getElementById("mensajePopup");
-    mensajePopup.style.display = "none";
-}
-
-// Agregar evento al botón "Cerrar"
-var cerrarPopup = document.getElementById("cerrarPopup");
-cerrarPopup.addEventListener("click", cerrarMensaje);
-
-// Manejar la respuesta del formulario
-var form = document.querySelector(".form");
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    fetch('tu_archivo_php.php', {
-        method: 'POST',
-        body: new FormData(form)
-    })
-    .then(response => response.json())
-    .then(data => {
-        mostrarMensaje(data.message, data.success);
-    })
-    .catch(error => {
-        mostrarMensaje('Error al procesar la solicitud.', false);
+        fetch('./php/formulario.php', {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => response.json())
+        .then(data => {
+            mostrarMensaje(data.message, data.success);
+        })
+        .catch(error => {
+            mostrarMensaje('Error al procesar la solicitud.', false);
+        });
     });
-});
+
